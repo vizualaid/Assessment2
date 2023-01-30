64e83c124fc1f2b0910e496d40c0cd6c06180dc7
vizualaid <isha.12011399@lpu.in>
2023-01-30 11:50:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+
 import Counter from "./components/Counter"
 function App() {
   return (
     <div className="App">
      <Counter></Counter>
+     <div>
+       <h4> Name: Isha Kunwar</h4>
+        
+        <h4>Registration No. : 12011399</h4>
+    </div>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Grid, Segment } from 'semantic-ui-react'
+import { Button, Card, Grid, Segment,Icon, Image  } from 'semantic-ui-react'
 
 function Counter() {
   const [count, setCount] = useState(0)
@@ -15,39 +15,43 @@ function Counter() {
   }
 
   return (
-    
-    <Segment >
-      <Grid columns={2} style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginTop:'5px'
-                }}>
-      <Grid.Row>
+
+    <div style={{padding:'40px',display: 'flex', justifyContent: 'center' }}>
+    <Segment placeholder  >
+   
+       
+      <Grid columns={2} style={{ justifyContent: 'center', margin:'5px' }} >
+        <Grid.Row >
           <Grid.Column>
-            <div style={{textAlign: "center"}}><h3>Count: {count}</h3></div>
+            <div>
+            <h2>Count </h2>
+            <h2>{count}</h2>
+            </div>
           </Grid.Column>
         </Grid.Row>
-        {/* <div  style={{textAlign:'center'}}>Count: {count}</div> */}
-        <Grid.Row style={{display: 'flex',  alignItems: 'center',  justifyContent: 'center',  marginTop:'5%' , marginBottom:'5%'  }}>
+       
+        <Grid.Row
+        style={{ marginTop:'5%' , marginBottom:'5%'  }}
+        >
 
           <Grid.Column>
             <Button color='green' onClick={handleIncrement}>Increment</Button>
           </Grid.Column>
 
           <Grid.Column>
-            <Button color='red' disabled={count === 0} onClick={handleDecrement}>Decrement</Button>
+            <Button color='red' 
+            disabled={count === 0} 
+            onClick={handleDecrement}>Decrement</Button>
           </Grid.Column>
         </Grid.Row>
 
         
       </Grid>
-      <div>
-       <h3> Name: ISHA KUNWAR</h3>
-        
-        <h3>Registration No. : 12011399</h3>
-      </div>
+
     </Segment>
+    
+
+    </div>
   )
 }
 
